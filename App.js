6bc432779f51efbdd118b5cce6ac236923370e44
Samuel Marques <samuelcabralmarques@gmail.com>
2021-07-07 22:33:05 -0300

    requisição POST

--- App.js
+++ App.js
@@ -1,21 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { SafeAreaView, FlatList, Text, View, Image } from 'react-native';
+
 
 export default function App() {
+
+
+  const [pokemons, setPokemons] = useState([])
+
+
+
+
+  useEffect(() => {
+    fetch('https://pokeapi.co/api/v2/pokemon', {
+      //executando o metodo GET dentro da api, para consulta
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json'
+      }
+    })
+    .then(Response => Response.json())
+    .then(data => {
+      setPokemons(data.results)
+    })
+  }, [])
+
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  
+      <SafeAreaView>
+        <FlatList 
+        data={pokemons}
+        keyExtractor={(pokemon) => pokemon.name }
+        contentContainerStyle={{ flexGrow: 1}}
+        renderItem={PokemonShow}
+        />
+      </SafeAreaView>
+  
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function PokemonShow(item){
+
+
+  const { name, url } = item.item
+ //na api a imagem esta separada do link original por isso estou usando o replace para subistituir
+ //o valor de uma url pela outra que contem a imagem, definido pelo ID de cada url para cada imagem referente
+
+  const pokemonNumber = url.replace('https://pokeapi.co/api/v2/pokemon/', '').replace('/', '')
+
+  const imageUrl = 'https://pokeres.bastionbot.org/images/pokemon/'+pokemonNumber+'.png'
+
+
+
+
+  return(
+    <View style={{ flexDirection: 'row'}} >
+      <Image style ={{width: 50, height: 50}} source={{ uri: imageUrl }}/>
+      <Text>{name}</Text>
+    </View>
+  )
+}
